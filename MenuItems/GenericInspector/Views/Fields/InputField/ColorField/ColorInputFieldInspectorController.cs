ab4ed9a23f5c467e2cc6a686ff01ac1450edaeb8
Darren Tsung <darren.tsung@gmail.com>
2017-08-09 09:51:59 -0700

    Add validation to Color field

--- MenuItems/GenericInspector/Views/Fields/InputField/ColorField/ColorInputFieldInspectorController.cs
+++ MenuItems/GenericInspector/Views/Fields/InputField/ColorField/ColorInputFieldInspectorController.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace DTDebugMenu.Internal {
 	public class ColorInputFieldInspectorController : IInputFieldInspectorController {
 		// PRAGMA MARK - IInputFieldInspectorController Implementation
+		char IInputFieldInspectorController.ValidateInput(string input, int charIndex, char addedChar) {
+			if (!charRegex_.IsMatch(addedChar.ToString())) {
+				return '\0';
+			}
+
+			return addedChar;
+		}
+
 		void IInputFieldInspectorController.HandleInputChanged(string input) {
 			if (!ColorUtil.IsStringValidHexColor(input)) {
 				return;
@@ -28,5 +37,6 @@ namespace DTDebugMenu.Internal {
 
 		// PRAGMA MARK - Internal
 		private IGenericInspectorField<Color> field_;
+		private Regex charRegex_ = new Regex("[0-9a-fA-F]");
 	}
 }
--- MenuItems/GenericInspector/Views/Fields/InputField/InputFieldInspector.cs
+++ MenuItems/GenericInspector/Views/Fields/InputField/InputFieldInspector.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 namespace DTDebugMenu.Internal {
 	public class InputFieldInspector : FieldInspector {
+		// PRAGMA MARK - Public Interface
 		public void Init(IInputFieldInspectorController controller) {
 			controller_ = controller;
 		}
@@ -21,10 +22,15 @@ namespace DTDebugMenu.Internal {
 		protected override void InternalInit() {
 			inputField_.text = controller_.GetCurrentValue();
 			inputField_.onValueChanged.AddListener(HandleValueChanged);
+			inputField_.onValidateInput += ValidateInput;
 		}
 
 		private void HandleValueChanged(string inputValue) {
 			controller_.HandleInputChanged(inputValue);
+		}
+
+		private char ValidateInput(string input, int charIndex, char addedChar) {
+			return controller_.ValidateInput(input, charIndex, addedChar);
 		}
 	}
 }
